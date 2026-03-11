b64502f0857ba970e3b6acd3e0d3f6e92c0cb8a8
mgiannaraki <mgiannaraki@vibestechnology.com>
2026-03-12 00:11:05 +0100

    Add FundaApiSettings, appsettings config and FundaApiClient skeleton

---
+++ FundaAssignment.ApiClient/FundaApiClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using FundaAssignment.Core.Interfaces;
+using FundaAssignment.Core.Models;
+
+namespace FundaAssignment.ApiClient
+{
+    public class FundaApiClient(HttpClient httpClient, FundaApiSettings settings) : IFundaApiClient
+    {
+        public async Task<IEnumerable<Listing>> GetAllListingsAsync(string searchQuery, CancellationToken cancellationToken = default)
+        {
+            string uri = settings.BaseUrl + "/" + settings.ApiKey  + searchQuery;
+
+            int totalNumOfPages = -1;
+            int currentPage = 1;
+            List<Listing> fullListOfListings = new();
+
+            do
+            {
+                string stringResponse = await httpClient.GetStringAsync(uri + "&page=" + currentPage + "&pagesize=" + settings.PageSize, cancellationToken);
+                FundaApiResponse? fundaApiResponse = JsonSerializer.Deserialize<FundaApiResponse>(stringResponse);
+                if(fundaApiResponse == null)
+                {
+                    break;
+                }
+
+                fullListOfListings.AddRange(fundaApiResponse.Objects);
+
+                if (totalNumOfPages == -1)
+                {
+                    totalNumOfPages = fundaApiResponse.Paging.AantalPaginas;
+                }
+
+                currentPage++;
+            } while (totalNumOfPages >= currentPage);
+
+            return fullListOfListings;
+        }
+    }
+}
